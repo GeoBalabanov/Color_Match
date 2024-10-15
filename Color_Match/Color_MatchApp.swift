6139f505c7ae404e19aae4b3afcdb999300a6acf
GeoBalabanov <georgibalabanov17d@gmail.com>
2024-10-15 11:02:21 +0200

    Initial Commit

---
+++ Color_Match/Color_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_MatchApp.swift
+//  Color_Match
+//
+//  Created by Georgi Balabanov on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Color_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color_MatchTests/Color_MatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_MatchTests.swift
+//  Color_MatchTests
+//
+//  Created by Georgi Balabanov on 15/10/2024.
+//
+
+import Testing
+@testable import Color_Match
+
+struct Color_MatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
